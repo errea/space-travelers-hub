7766fbd5f17205c530b09cdc092e7a382c9b5584
Alonso Falconi <alonso.fl@live.com>
2021-08-30 14:53:52 -0500

    Add Rockets component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
 
 const App = () => (
   <BrowserRouter>
     <Header />
+    <main>
+      <Route exact path="/">
+        <Rockets />
+      </Route>
+    </main>
   </BrowserRouter>
 );
 
