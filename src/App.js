f2fa44a69a7234bae93bc415702d653632a42591
errea <erreakezy@gmail.com>
2021-08-30 21:24:54 +0100

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Rockets from './components/Missions';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
 
 const App = () => (
   <BrowserRouter>
     <Header />
-    <Missions />
+    <main>
+      <Route exact path="/">
+        <Rockets />
+      </Route>
+      <Route path="/missions">
+        <Missions />
+      </Route>
+    </main>
   </BrowserRouter>
 );
 
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,5 @@
 const Missions = () => (
-    <h2>Missions List View</h2>
-  );
-  
-  export default Missions;
+  <h2>Missions List View</h2>
+);
+
+export default Missions;
