727f1e46586250a39ce87c98ecb7b8f7c29fdd6f
errea <erreakezy@gmail.com>
2021-08-30 21:07:59 +0100

    Modify App.js to use export mission function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Missions';
 
 const App = () => (
   <BrowserRouter>
     <Header />
+    <Missions />
   </BrowserRouter>
 );
 
