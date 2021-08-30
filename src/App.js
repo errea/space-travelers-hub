47499e47082a4e67c53d3cf27210314a3c869776
Alonso Falconi <alonso.fl@live.com>
2021-08-30 16:03:21 -0500

    Configure Redux store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <main>
-      <Route exact path="/">
-        <Rockets />
-      </Route>
-      <Route path="/missions">
-        <Missions />
-      </Route>
-    </main>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Route exact path="/">
+          <Rockets />
+        </Route>
+        <Route path="/missions">
+          <Missions />
+        </Route>
+      </main>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
