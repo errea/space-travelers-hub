cce6529cc9c99b32c9a51ae0b0b77cbc7dfcdae1
errea <erreakezy@gmail.com>
2021-08-31 09:51:52 +0100

    Add function to dispatch and update redux store

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,40 @@
-const Missions = () => (
-  <h2>Missions List View</h2>
-);
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getInfo, updateState } from '../redux/missions/missions';
 
-export default Missions;
+const Mission = () => {
+  const dispatch = useDispatch();
+  const missionData = useSelector((state) => state.missionReducer);
+  useEffect(() => {
+    dispatch(retrieveInfo([]));
+  }, []);
+
+  const handleMissionStateClick = (key, status) => {
+    if (status) {
+      dispatch(updateState({
+        id: key,
+        value: false,
+      }));
+    } else {
+      dispatch(updateState({
+        id: key,
+        value: true,
+      }));
+    }
+  };
+
+  return (
+    <div>
+      {missionData.map((data) => (
+        <div key={data.mission_id}>
+          <div>{data.mission_name}</div>
+          <div>{data.description}</div>
+          <div><div>{data.mission_status ? 'Active Member' : 'NOT A MEMBER'}</div></div>
+          <div><button onClick={() => { handleMissionStateClick(data.mission_id, data.mission_status); }} type="button" id="member_btn">{data.mission_status ? 'Leave Mission' : 'Join Mission'}</button></div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Mission;
