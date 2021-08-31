e22ea544cc70dbbcd78a11a859a59ecf84a73046
Alonso Falconi <alonso.fl@live.com>
2021-08-30 21:43:25 -0500

    Fetch data from API

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,17 @@
-const Rockets = () => (
-  <h2>Rockets List</h2>
-);
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets);
+  });
+
+  return (
+    <h1>SPACE</h1>
+  );
+};
 
 export default Rockets;
