a419aa10571828b04499df499cd62e0f45985fff
Alonso Falconi <alonso.fl@live.com>
2021-08-31 12:11:19 -0500

    Add rockets list style

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,11 +1,13 @@
 const RocketItem = (prop) => {
-  const { name, image } = prop;
+  const { name, description, image } = prop;
 
   return (
     <li>
       <img src={image} alt="Rocket" />
       <div className="rocket-info">
         <h2>{name}</h2>
+        <p>{description}</p>
+        <button type="button">Reserve Rocket</button>
       </div>
     </li>
   );
