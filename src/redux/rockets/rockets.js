335e03c1d456ef2374cbc5bca564bf939dc75521
Alonso Falconi <alonso.fl@live.com>
2021-08-31 14:53:45 -0500

    Add reducer logic to handle rocket reservation

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -9,8 +9,19 @@ const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return action.payload;
-    case RESERVE_ROCKET:
-      return state;
+    case RESERVE_ROCKET: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+        return {
+          ...rocket,
+          reserved: true,
+        };
+      });
+
+      return newState;
+    }
     default:
       return state;
   }
