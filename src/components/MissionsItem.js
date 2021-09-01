1659738830002bdb92a4d892db67c0c7982e67f4
errea <erreakezy@gmail.com>
2021-09-01 16:01:32 +0100

    Modify MissionsItem.js file + add extra conditional for the switch badge button

--- src/components/MissionsItem.js
+++ src/components/MissionsItem.js
@@ -1,13 +1,20 @@
 const MissionsItem = (prop) => {
   const {
-    joinMissionHandler, id, name, description, join, leaveMissionHandler,
+    joinMissionHandler, id, name, description, reserved, leaveMissionHandler,
   } = prop;
 
   let button;
-  if (!join) {
-    button = <button type="button" onClick={() => joinMissionHandler(id)}>Join Mission</button>;
+  if (!reserved) {
+    button = <button type="button" className="mission-button-join" onClick={() => joinMissionHandler(id)}>Join Mission</button>;
   } else {
-    button = <button type="button" className="mission-button-join" onClick={() => leaveMissionHandler(id)}>Leave Mission</button>;
+    button = <button type="button" className="mission-button-badge" onClick={() => leaveMissionHandler(id)}>Leave Mission</button>;
+  }
+
+  let badge;
+  if (!reserved) {
+    badge = <span className="mission-button">Not a Member</span>;
+  } else {
+    badge = <span className="join-badge ">Active Member</span>;
   }
 
   return (
@@ -16,25 +23,10 @@ const MissionsItem = (prop) => {
       <td className="mission-title">{name}</td>
       <td className="mission-description">{description}</td>
       <td>
-        <span className="mission-button">
-          Not a Member
-        </span>
+        {badge}
       </td>
       <td>
-
-        <p>
-          {join && (<span className="join-badge">Join Mission</span>)}
-          {description}
-        </p>
         {button}
-        {/* <button className="mission-button-join"
-        type="button" onClick={() => joinMissionHandler(id)}>
-          Join Mission
-        </button>
-        <button className="mission-button-join"
-        type="button" onClick={() => leaveMissionHandler(id)}>
-          Leave Mission
-        </button> */}
       </td>
     </tr>
 
